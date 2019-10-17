68972c0490d4e16e17d00d23097f95c8d31e36d9
yousj <youshaojunde@163.com>
2019-10-17 18:02:41 +0800

    优化代码

--- provider/src/main/java/com/cloud/provider/config/DataSourceConfig.java
+++ provider/src/main/java/com/cloud/provider/config/DataSourceConfig.java
@@ -16,46 +16,48 @@ public class DataSourceConfig {
 
     @Autowired
     private Environment env;
+    private static final String URL = "url";
+    private static final String USERNAME = "username";
+    private static final String PASSWORD = "password";
+    private static final String DRIVER_CLASSNAME = "driverClassName";
     private static final String PREFIX_01 = "spring.datasource.test01.";
     private static final String PREFIX_02 = "spring.datasource.test02.";
     private static final String PREFIX_03 = "spring.datasource.test03.";
+    private static final String RESOURCE_NAME_01 = "test01";
+    private static final String RESOURCE_NAME_02 = "test02";
+    private static final String RESOURCE_NAME_03 = "test03";
+    private static final String XA_DATASOURCE_CLASSNAME = "com.alibaba.druid.pool.xa.DruidXADataSource";
 
     @Bean(name = "dataSource01")
     public DataSource dataSource01() throws Exception {
-        AtomikosDataSourceBean ds = new AtomikosDataSourceBean();
-        ds.setXaDataSourceClassName("com.alibaba.druid.pool.xa.DruidXADataSource");
-        ds.setUniqueResourceName("test01");
-        ds.setPoolSize(5);
-        ds.setXaProperties(build(PREFIX_01));
-        return ds;
+        return setAtomikosParams(RESOURCE_NAME_01, PREFIX_01);
     }
 
     @Bean(name = "dataSource02")
     public DataSource dataSource02() throws Exception {
-        AtomikosDataSourceBean ds = new AtomikosDataSourceBean();
-        ds.setXaDataSourceClassName("com.alibaba.druid.pool.xa.DruidXADataSource");
-        ds.setUniqueResourceName("test02");
-        ds.setPoolSize(5);
-        ds.setXaProperties(build(PREFIX_02));
-        return ds;
+        return setAtomikosParams(RESOURCE_NAME_02, PREFIX_02);
     }
 
     @Bean(name = "dataSource03")
     public DataSource dataSource03() throws Exception {
+        return setAtomikosParams(RESOURCE_NAME_03, PREFIX_03);
+    }
+
+    public DataSource setAtomikosParams(String resourceName, String prefix) {
         AtomikosDataSourceBean ds = new AtomikosDataSourceBean();
-        ds.setXaDataSourceClassName("com.alibaba.druid.pool.xa.DruidXADataSource");
-        ds.setUniqueResourceName("test03");
+        ds.setXaDataSourceClassName(XA_DATASOURCE_CLASSNAME);
+        ds.setUniqueResourceName(resourceName);
         ds.setPoolSize(5);
-        ds.setXaProperties(build(PREFIX_03));
+        ds.setXaProperties(build(prefix));
         return ds;
     }
 
     private Properties build(String prefix) {
         Properties prop = new Properties();
-        prop.put("url", env.getProperty(prefix + "url"));
-        prop.put("username", env.getProperty(prefix + "username"));
-        prop.put("password", env.getProperty(prefix + "password"));
-        prop.put("driverClassName", env.getProperty(prefix + "driverClassName"));
+        prop.put(URL, env.getProperty(prefix + URL));
+        prop.put(USERNAME, env.getProperty(prefix + USERNAME));
+        prop.put(PASSWORD, env.getProperty(prefix + PASSWORD));
+        prop.put(DRIVER_CLASSNAME, env.getProperty(prefix + DRIVER_CLASSNAME));
         return prop;
     }
 }
